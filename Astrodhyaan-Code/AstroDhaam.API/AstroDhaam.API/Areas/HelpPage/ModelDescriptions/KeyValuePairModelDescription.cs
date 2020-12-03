c6296f026f2a33868035808b9145ff2ffb414f06
kuns987 <46552294+kuns987@users.noreply.github.com>
2020-12-03 17:55:02 +0530

    Main Code

---
+++ Astrodhyaan-Code/AstroDhaam.API/AstroDhaam.API/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace AstroDhaam.API.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
